b023d2c4390ef8e5af0b8c651a60e9b938d2bf40
kmpery <perrikembali@gmail.com>
2025-05-01 18:10:09 +0800

    c

--- app.js
+++ app.js
@@ -73,9 +73,9 @@ wss.on('connection', (ws) => {
   ws.on('message', (data) => {
     console.log('Pesan masuk:', data.toString());
 
-    // Broadcast ke semua klien (kecuali pengirim)
+    // Kirim ke semua klien, termasuk pengirim
     wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(data.toString());
       }
     });
